e2b950d50a4f0b61cd8983682d14d5f5ead2fc27
Indira <indira.kabdoldina@kaspi.kz>
2025-05-30 22:18:45 +0500

    feat: output of enums

---
+++ java/src/main/java/org/cases/objects/RegularEnumSet.java
@@ -0,0 +1,24 @@
+package org.cases.objects;
+
+/**
+ *
+ * returns the instance of RegularEnumSet if elements < 64
+**/
+public enum RegularEnumSet {
+
+    A("letter A"),
+    B("letter B"),
+    C("letter C"),
+    D("letter D");
+
+    private final String letter;
+
+    private RegularEnumSet(String letter) {
+        this.letter = letter;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+}
