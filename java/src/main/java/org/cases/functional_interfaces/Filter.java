bf236d96a853741a1ce5caa700ad4f7ebc28bca6
Indira <indira.kabdoldina@kaspi.kz>
2025-06-16 15:36:35 +0500

    feat: use case with predicate and function interfaces

---
+++ java/src/main/java/org/cases/functional_interfaces/Filter.java
@@ -0,0 +1,36 @@
+package org.cases.functional_interfaces;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Filter {
+
+    public List<String> filterWords(List<String> words) {
+        return words.stream()
+                .filter(word -> word.length() > 5)
+                .map(String::toUpperCase)
+                .toList();
+
+    }
+
+    /**
+     *
+     *filterWords(List.of("apple", "banana", "pear", "avocado"),
+     *     word -> word.startsWith("a"),
+     *     String::toUpperCase)
+     */
+    public List<String> filterWords(List<String> words,
+                                    Predicate<String> predicate,
+                                    Function<String, String> function) {
+
+        return words.stream().filter(predicate)
+                .map(function)
+                .toList();
+    }
+
+
+
+
+
+}
