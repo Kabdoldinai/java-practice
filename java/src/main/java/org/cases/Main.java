3b9d1dd3de1416bffc258ce1296df3bcfb820375
Indira <indira.kabdoldina@kaspi.kz>
2025-07-08 21:16:57 +0500

    feat: add mini thread tasks

--- java/src/main/java/org/cases/Main.java
+++ java/src/main/java/org/cases/Main.java
@@ -1,14 +1,8 @@
 package org.cases;
 
-import org.cases.multithreading.HelloFromThreads;
-
 public class Main {
 
-    public static void main(String[] args) {
-
-        HelloFromThreads helloFromThreads = new HelloFromThreads();
-
-        helloFromThreads.printHelloByFiveThreads();
+    public static void main(String[] args) throws InterruptedException {
 
     }
 }
